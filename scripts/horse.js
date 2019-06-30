f140d670de9b753f85f8914c41827b63da3deff7
Kevin Ring <kevin@kotachrome.com>
2019-06-30 15:35:34 +1000

    Moon dodging.

--- scripts/horse.js
+++ scripts/horse.js
@@ -31,6 +31,9 @@ addEventListener('click', function(event) {
     neigh.play();
 }, false);
 
+let horseX;
+let horseY;
+
 function moveTo(x, y) {
     x -= horseWidth / 2;
     y -= horseHeight / 2;
@@ -53,6 +56,9 @@ function moveTo(x, y) {
     if (y < 0) {
         y = 0;
     }
+
+    horseX = x;
+    horseY = y;
 
     const horse = document.getElementsByClassName('horse')[0];
     horse.style.left = x + 'px';
@@ -97,3 +103,75 @@ setInterval(function() {
     const image = backgrounds[currentImage];
     page.style['background-image'] = `url(${image})`;
 }, 5000);
+
+const page = document.getElementsByClassName('page')[0];
+const height = page.clientHeight;
+const width = height * 10;
+
+const levelWidth = width;
+const startTime = performance.now();
+
+const obstacles = [];
+const obstacleDomElements = [];
+
+function createLevel() {
+    
+    const numberOfMoons = 18;
+    for (let i = 0; i < numberOfMoons; ++i) {
+        const x = (width - 150) * Math.random() - height * 9;
+        const y = (height - 158) * Math.random();
+
+        const obstacle = document.createElement("div");
+        obstacle.className = "obstacle";
+        obstacle.style.left = x + 'px';
+        obstacle.style.top = y + 'px';
+        obstacle.innerHTML = '<img width="150" src="images/moon-small.png" />';
+        obstacleDomElements.push(obstacle);
+        obstacles.push([x, y]);
+        page.appendChild(obstacle);
+    }    
+}
+
+createLevel();
+
+function run() {
+    const currentTime = performance.now() - startTime;
+    const currentLevelPosition = (currentTime / 2) % levelWidth;
+
+    const horseRectangle = {
+        minX: horseX,
+        minY: horseY,
+        maxX: horseX + horseWidth,
+        maxY: horseY + horseHeight
+    };
+
+    for (let i = 0; i < obstacles.length; ++i) {
+        const obstacle = obstacles[i];
+        const domElement = obstacleDomElements[i];
+        const newPosition = obstacle[0] + currentLevelPosition;
+        domElement.style.left = newPosition + "px";
+
+        const obstacleRectangle = {
+            minX: newPosition,
+            minY: obstacle[1],
+            maxX: newPosition + 150,
+            maxY: obstacle[1] + 157
+        };
+
+        const intersection = {
+            minX: Math.max(obstacleRectangle.minX, horseRectangle.minX),
+            maxX: Math.min(obstacleRectangle.maxX, horseRectangle.maxX),
+            minY: Math.max(obstacleRectangle.minY, horseRectangle.minY),
+            maxY: Math.min(obstacleRectangle.maxY, horseRectangle.maxY)
+        };
+
+        if (intersection.minX < intersection.maxX && intersection.minY < intersection.maxY) {
+            // Collision!
+            neigh.play();
+        }
+    }
+    
+    requestAnimationFrame(run);
+}
+
+requestAnimationFrame(run);
